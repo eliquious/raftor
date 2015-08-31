15cd621659da90c777c923048e7b9984eab18bb9
eliquious <max.franks@synapse-wireless.com>
2015-08-31 01:31:26 -0500

    Add docs to Sender interface

--- sender.go
+++ sender.go
@@ -3,5 +3,11 @@ package raftor
 import "github.com/coreos/etcd/raft/raftpb"
 
 type Sender interface {
+
+	// Send sends out the given messages to the remote peers.
+	// Each message has a To field, which is an id that maps
+	// to an existing peer in the transport.
+	// If the id cannot be found in the transport, the message
+	// will be ignored.
 	Send([]raftpb.Message)
 }
