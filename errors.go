8873ef33320ff84cd14846eb3bd1877eceda6f5e
eliquious <max.franks@synapse-wireless.com>
2015-08-30 21:53:47 -0500

    Add errors

---
+++ errors.go
@@ -0,0 +1,9 @@
+package raftor
+
+import "errors"
+
+var (
+	ErrCanceled               = errors.New("raft: request cancelled")
+	ErrTimeout                = errors.New("raft: request timed out")
+	ErrTimeoutDueToLeaderFail = errors.New("raft: request timed out, possibly due to previous leader failure")
+)
